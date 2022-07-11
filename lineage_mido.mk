9aae580396e34aba4813637f452bbbce6c3fa2ac
shubhojitnag <shubhojitnag2009@gmail.com>
2022-07-11 13:45:04 +0530

    mido : Fork los 19 flags

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -24,6 +24,10 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Fork Los Flags
+TARGET_FLOS := true
+#WITH_GMS := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := lineage_mido
